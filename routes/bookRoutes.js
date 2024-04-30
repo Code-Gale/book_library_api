dad64348ceb417881311bcb9409e9312cd60b504
Code-Gale <codewhiz001@gmail.com>
2024-04-30 15:28:20 +0100

    routes for book_library

---
+++ routes/bookRoutes.js
@@ -0,0 +1,12 @@
+const route = require('express').Router()
+
+//getting all books
+route.get('/books', )
+//adding books
+route.post('/books', )
+//search for a book by title
+route.get('/books/authors/:author', )
+//update book details
+route.put('/books/:id', )
+//delete a book
+route.delete('/books/:id', )
