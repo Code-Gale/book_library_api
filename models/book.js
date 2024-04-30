6163808cea21ea310a0b7f42159f0e36f17c8482
Code-Gale <codewhiz001@gmail.com>
2024-04-30 15:25:12 +0100

    schema and model for book libraries

---
+++ models/book.js
@@ -0,0 +1,26 @@
+const { Timestamp } = require('mongodb');
+const mongoose = require('mongoose')
+
+const Schema = express.Schema;
+
+const bookSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    author : {
+        type : String,
+        required : true,
+    },
+    year : {
+        type : Number,
+        required : true
+    },
+    categories : {
+        type : String,
+        required : true
+    }
+}, { Timestamp : true})
+
+const Book = mongoose.model('book', bookSchema)
+module.exports = Book
