ce5562374823508f9e85217f56b7d8ead7b7f796
Code-Gale <codewhiz001@gmail.com>
2024-04-30 16:51:46 +0100

    finished routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,12 +1,17 @@
 const route = require('express').Router()
 
+const bookController = require('../controllers/bookController')
 //getting all books
-route.get('/books', )
+route.get('/books', bookController.get_books)
 //adding books
-route.post('/books', )
+route.post('/books', bookController.add_book)
 //search for a book by title
-route.get('/books/authors/:author', )
+route.get('/books/titles/:title', bookController.find_book_by_title)
+//search for book by author
+route.get('/books/authors/:author', bookController.find_book_by_author)
 //update book details
-route.put('/books/:id', )
+route.put('/books/:id', bookController.update_book)
 //delete a book
-route.delete('/books/:id', )
+route.delete('/books/:id', bookController.delete_book)
+
+module.exports = route;
